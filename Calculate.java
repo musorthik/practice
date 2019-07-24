937f738fcf38e548d0f513747b17271ad22f3463
Volodymyr <musorthik@gmail.com>
2019-07-24 16:28:29 +0300

    First commit

---
+++ Calculate.java
@@ -0,0 +1,12 @@
+public class Calculate {
+
+	public static void main (String [] arg){
+
+		int first = Integer.valueOf(arg[0]);
+		int second = Integer.valueOf(arg[1]);
+		int sum = first + second;
+		System.out.println("Sum: " + sum);
+
+	}
+
+}
